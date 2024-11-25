546477b1c9506f2e5ae20c2c410936bbfc6b01f0
ParvathiMN <parvathi.m2004@gmail.com>
2024-11-25 10:32:40 +0400

    Web app

---
+++ products.js
@@ -0,0 +1,91 @@
+let products =[
+    { id:1000,
+        subject:"Rugby ",
+        Location: "London",
+        price : 150,
+        availableslots: 5,
+        image : "image/1.jpg",
+        rating: 3
+        },
+{
+    id:1002,
+    subject:"Language",
+    Location: "Delhi",
+    price : 200,
+    availableslots: 5 ,
+    image : "image/Lang-removebg-preview.png"  ,
+    rating: 4 
+},
+{
+    id:1003,
+    subject:"Swimming",
+    Location: "Nice",
+    price : 250,
+    availableslots: 5 ,
+    image : "image/s.jpeg"  ,
+    rating: 5 
+},
+{
+    id:1004,
+    subject:" Debate",
+    Location: "Ottowa",
+    price : 360,
+    availableslots: 5 ,
+    image : "image/d.png"  ,
+    rating: 3
+},
+{
+    id:1005,
+    subject:"Indian Classical Dance",
+    Location: "Chennai",
+    price : 180,
+    availableslots: 5 ,
+    image : "image/A.jpg"  ,
+    rating: 4 
+},
+{
+    id:1006,
+    subject:"Painting Lessons",
+    Location: "Paris",
+    price : 350,
+    availableslots: 5 ,
+    image : "image/art.jpeg"  ,
+    rating: 3 
+},
+{
+    id:1007,
+    subject:"Drums Lessions",
+    Location: "Berlin",
+    price : 170,
+    availableslots: 5 ,
+    image : "image/drums.jpg"  ,
+    rating: 3 
+},
+{
+    id:1008,
+    subject:"Tennis Lessions",
+    Location: "Canberra",
+    price : 100,
+    availableslots: 5 ,
+    image : "image/tennis.jpeg"  ,
+    rating: 3 
+},
+{
+    id:1009,
+    subject:"Kickboxing Lessions",
+    Location: "Boston",
+    price : 350,
+    availableslots: 5 ,
+    image : "image/9.jpg"  ,
+    rating: 4 
+},
+{
+    id:1010,
+    subject:"Cyber security",
+    Location: "Washington",
+    price : 450,
+    availableslots: 5 ,
+    image : "image/cyber-removebg-preview.png"  ,
+    rating: 4
+},
+]
